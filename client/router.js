e3f85f593ffabe19b58dae6615b46b85c7916870
limingth <limingth@gmail.com>
2015-02-17 00:43:45 -0800

    Upgrade to 1.0.3.1

--- client/router.js
+++ client/router.js
@@ -2,6 +2,12 @@
   '/':'homepage',
   '/calendar': 'calendar'
 })*/
+
+
+Router.configure({
+  layoutTemplate: 'layout'
+});
+
 Router.map(function(){
   this.route('homepage', {path:'/'});
   this.route('calendar', {path:'/calendar'});
